d6df749d7c7d5235520357f385ce97c27a1c32c2
Vamiyn <vamiynr@gmail.com>
2018-01-27 19:17:09 +0200

    added animation and an animator controller for the fawn

---
+++ Assets/_Scripts/LightBeamController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBeamController : MonoBehaviour {
+
+	List<GameObject> rightLights;
+	List<GameObject> leftLights;
+
+	bool lightsMoving = true;
+
+	void Start () {
+
+		rightLights = new List<GameObject>();
+		foreach (Transform obj in transform.Find("Right")) {
+			rightLights.Add(obj.gameObject);
+		}
+
+		leftLights = new List<GameObject>();
+		foreach (Transform obj in transform.Find("Left")) {
+			leftLights.Add(obj.gameObject);
+		}
+
+		lightsMoving = false;
+		
+	}
+	void Update () {
+		if (!lightsMoving) {
+			if (Input.GetKeyDown(KeyCode.P)) {
+				StartCoroutine(WaveFromFront());
+			}
+		}
+	}
+
+	IEnumerator WaveFromFront() {
+		yield return new WaitForSeconds(0.1f);
+		for (int i = 0; i < 5; i++) {
+			
+		}
+	}
+
+}
