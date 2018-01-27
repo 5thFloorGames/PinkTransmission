d6d95cae72800ad3571c1c40c13ac4df9818b193
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2018-01-27 18:53:31 +0200

    Tiles now have separate objects for different looks

--- Assets/_Scripts/TileColor.cs
+++ Assets/_Scripts/TileColor.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class TileColor : MonoBehaviour {
 
 	public TileState tileState = TileState.Neutral;
+	public GameObject neutral;
+	public GameObject animal;
+	public GameObject shroom;
+	private Dictionary<TileState,GameObject> tiles;
 
 	// Use this for initialization
 	void Start () {
+		tiles = new Dictionary<TileState, GameObject>();
+		tiles.Add(TileState.Neutral,neutral);
+		tiles.Add(TileState.Animal,animal);
+		tiles.Add(TileState.Shroom,shroom);
 		tileState = TileState.Neutral;
+		EnableState();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(tileState == TileState.Animal){
-			transform.Find("Sprite").GetComponent<SpriteRenderer>().color = Color.red;
-		} else if(tileState == TileState.Shroom){
-			transform.Find("Sprite").GetComponent<SpriteRenderer>().color = Color.green;
-		}
+	}
+
+	void EnableState(){
+		tiles[tileState].SetActive(true);
+	}
+
+	void DisableState(){
+		tiles[tileState].SetActive(false);
 	}
 
 	public void ChangeOwner(TileState state){
+		DisableState();
 		tileState = state;
+		EnableState();
 	}
 }
