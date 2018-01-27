3b7c5fb62d2b2ee83470334dbe4fc87644e41bb3
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2018-01-27 12:51:39 +0200

    merge and adding tiles

---
+++ Assets/_Scripts/GenerateBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBoard : MonoBehaviour {
+
+	int YSize = 9;
+	int XSize = 9;
+	GameObject tile;
+	TileColor [,] tiles;
+
+	// Use this for initialization
+	void Start () {
+		tiles = new TileColor[XSize * 2, YSize *2];
+		tile = Resources.Load<GameObject>("Tile");
+		for(int i = 0;i<=XSize;i++){
+			for(int j = 0;j<=YSize;j++){
+				GameObject g = Instantiate(tile,transform);
+				g.transform.position = new Vector3(i - 5,j - 5,0);
+				tiles[i,j] = g.GetComponent<TileColor>();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/TileColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileColor : MonoBehaviour {
+
+	public TileState tileState = TileState.Neutral;
+
+	// Use this for initialization
+	void Start () {
+		if(Random.Range(0f,1f) < 0.5f){
+			tileState = TileState.Animal;
+		} else {
+			if(Random.Range(0f,1f) < 0.5f){	
+				tileState = TileState.Shroom;
+			} else {
+				tileState = TileState.Neutral;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(tileState == TileState.Animal){
+			GetComponent<SpriteRenderer>().color = Color.red;
+		} else if(tileState == TileState.Shroom){
+			GetComponent<SpriteRenderer>().color = Color.green;
+		}
+	}
+}
