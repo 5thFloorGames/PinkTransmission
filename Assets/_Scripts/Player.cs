8beba32788ec1289efb9a1d933402e88dc5a99e7
zanagi <yuanqi.shan@gmail.com>
2018-01-27 10:42:57 +0200

    CameraObject prefab

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerID
+{
+    A, B
+}
+
 public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private PlayerID id;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +19,12 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CheckInput();
 	}
+
+    private void CheckInput()
+    {
+        var dx = Input.GetAxis("Horizontal" + id);
+        var dy = Input.GetAxis("Vertical" + id);
+    }
 }
