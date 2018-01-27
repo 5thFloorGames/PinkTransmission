82f937043d86a729850b6105d17b496885424aa3
zanagi <yuanqi.shan@gmail.com>
2018-01-27 12:25:54 +0200

    Clean out old wwise

---
+++ Assets/_Scripts/UI/ShroomSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShroomSlot : MonoBehaviour {
+
+    public Shroom shroom;
+    private Image iconImage;
+
+	// Use this for initialization
+	void Start () {
+        iconImage = transform.Find("Image").GetComponent<Image>();
+	}
+	
+	public void SetShroom(Shroom shroom)
+    {
+        this.shroom = shroom;
+        iconImage.sprite = shroom.icon;
+    }
+}
---
+++ Assets/_Scripts/UI/ShroomSlotManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShroomSlotManager : MonoBehaviour {
+
+    [SerializeField]
+    private Shroom[] shroomPrefabs;
+    private ShroomSlot[] slots;
+
+	// Use this for initialization
+	void Start ()
+    {
+        slots = GetComponentsInChildren<ShroomSlot>();
+        
+        for(int i = 0; i < slots.Length; i++)
+        {
+            slots[i].SetShroom(Static.GetRandom(shroomPrefabs));
+        }
+    }
+}
