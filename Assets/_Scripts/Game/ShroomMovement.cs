a4ee59aee665e7f4988049450f6416dda5da2e21
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2018-01-28 12:42:34 +0200

    Shrrom beatmatched moevement works

--- Assets/_Scripts/Game/ShroomMovement.cs
+++ Assets/_Scripts/Game/ShroomMovement.cs
@@ -6,38 +6,65 @@ public class ShroomMovement : MonoBehaviour {
 
     int i = 0;
     Animator animator;
+
+    MusicManager music;
+
+    bool jumpedAlready = false;
     // Use this for initialization
     void Start()
     {
+        music = FindObjectOfType<MusicManager>();
         MusicManager.OnBeat += Move;
         animator = GetComponentInChildren<Animator>();
     }
     
+    private void Update(){
+        if(!jumpedAlready){
+            if(Input.GetKeyDown(KeyCode.UpArrow)){
+                if(music.CloseToHalfBar()){
+                    GenerateBoard.Instance.MoveShroom(MoveDirection.Up);
+                    StartCoroutine(ResetMove());
+                }
+            }
+            if(Input.GetKeyDown(KeyCode.DownArrow)){
+                if(music.CloseToHalfBar()){
+                    GenerateBoard.Instance.MoveShroom(MoveDirection.Down);
+                    StartCoroutine(ResetMove());
+                }
+            }
+            if(Input.GetKeyDown(KeyCode.LeftArrow)){
+                if(music.CloseToHalfBar()){
+                    GenerateBoard.Instance.MoveShroom(MoveDirection.Right);
+                    StartCoroutine(ResetMove());
+                }
+            }
+            if(Input.GetKeyDown(KeyCode.RightArrow)){
+                if(music.CloseToHalfBar()){
+                    GenerateBoard.Instance.MoveShroom(MoveDirection.Left);
+                    StartCoroutine(ResetMove());
+                }
+            }
+            if(Input.GetKeyDown(KeyCode.Space)){
+                if(music.CloseToHalfBar()){
+                    GenerateBoard.Instance.SpawnShroom();
+                    ShroomStinger();
+                    StartCoroutine(ResetMove());
+                }
+            }
+        }
+    }
+
+    IEnumerator ResetMove(){
+		jumpedAlready = true;
+		yield return new WaitForSeconds(music.TimeToNextBeat() + 0.1f);
+		jumpedAlready = false;
+	}
+
     private void Move()
     {
         if (i++ % 2 == 0)
         {
             animator.SetTrigger("Jump");
-            if (Input.GetAxis("HorizontalB") > 0)
-            {
-                GenerateBoard.Instance.MoveShroom(MoveDirection.Left);
-            }
-            else if (Input.GetAxis("HorizontalB") < 0)
-            {
-                GenerateBoard.Instance.MoveShroom(MoveDirection.Right);
-            }
-            else if (Input.GetAxis("VerticalB") > 0)
-            {
-                GenerateBoard.Instance.MoveShroom(MoveDirection.Up);
-            }
-            else if (Input.GetAxis("VerticalB") < 0)
-            {
-                GenerateBoard.Instance.MoveShroom(MoveDirection.Down);
-            } else if(Input.GetAxis("SubmitB") > 0)
-            {
-                ShroomStinger();
-                GenerateBoard.Instance.SpawnShroom();
-            }
         }
     }
 
